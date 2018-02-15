b16d8dab01fb37a3c79f47ca5a61e1925287553a
Hunter Wright <wright.hunter@gmail.com>
2018-02-15 08:31:50 -0600

    Added App.config to gitignore.

--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 
 namespace SqlIntro
 {
@@ -6,17 +7,17 @@ namespace SqlIntro
     {
         static void Main(string[] args)
         {
-            var connectionString = "Server=localhost;Database=adventureworks;Uid=root;Pwd=password;"; //get connectionString format from connectionstrings.com and change to match your database
+            var connectionString = "Server=localhost;Database=adventureworksw;Uid=root;Pwd=password;"; //get connectionString format from connectionstrings.com and change to match your database
             var repo = new ProductRepository(connectionString);
             foreach (var prod in repo.GetProducts())
             {
                 Console.WriteLine("Product Name:" + prod.Name);
             }
 
-           
+
             Console.ReadLine();
         }
 
-       
+
     }
 }
